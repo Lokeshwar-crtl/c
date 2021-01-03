d10959c3b115b6dfe1d35f5564e2383d45801180
Lokeshwar-crtl <76592546+Lokeshwar-crtl@users.noreply.github.com>
2021-01-03 19:39:25 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -3,9 +3,10 @@ void main()
 {
 int i,j;
 int arr[10];
+printf("the elements are:\n);
 for(i=0;i<=9;i++)
 for(j=1;j<=10;j++)
 arr[i]=j;
 for(i=0;i<=j;i++)
-printf("the elements are :%d\n",arr[i]);
+printf("%d\n",arr[i]);
 }
