91b1c85323c57d2ce403627f4ebc1a2a682adf08
Lokeshwar-crtl <76592546+Lokeshwar-crtl@users.noreply.github.com>
2021-01-03 19:29:44 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -3,9 +3,10 @@ void main()
 {
 int i;
 int arr[10];
-printf("enter the values : ");
+printf("enter the values \n");
 for(i=0;i<=9;i++)
-scanf("%d",&arr[i]);
+{scanf("%d",&arr[i]);}
+printf("entered values are : ");
 for(i=0;i<=9;i++)
-printf("%d",arr[i]);
+printf("%d, ",arr[i]);
 }
